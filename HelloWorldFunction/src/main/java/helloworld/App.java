d07e5255706f0bce3e4717583db9ea5d227bf815
Keshav <keshavkb@amazon.com>
2021-12-29 10:19:41 +0800

    Launched glue job via local invoke

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,12 +1,16 @@
 package helloworld;
 
-import java.io.*;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import schema.aws.s3.objectcreated.AWSEvent;
 import schema.aws.s3.objectcreated.ObjectCreated;
 import schema.aws.s3.objectcreated.marshaller.Marshaller;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
+import software.amazon.awssdk.services.glue.GlueAsyncClient;
+import software.amazon.awssdk.services.glue.model.StartJobRunRequest;
+
+import java.io.*;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -15,6 +19,12 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 public class App implements RequestStreamHandler {
 
+    private static final SdkAsyncHttpClient sdkAsyncHttpClient = AwsCrtAsyncHttpClient.create();
+
+    private static final GlueAsyncClient glueClient = GlueAsyncClient.builder()
+                                                           .httpClient(sdkAsyncHttpClient)
+                                                           .build();
+
     private Object handleEvent(final AWSEvent<ObjectCreated> inputEvent, final Context context) {
         if (inputEvent == null) {
             return inputEvent;
@@ -22,7 +32,7 @@ public class App implements RequestStreamHandler {
 
         ObjectCreated objectCreated = inputEvent.getDetail();
 
-        if(objectCreated.getBucket() == null) {
+        if (objectCreated.getBucket() == null) {
             log("S3 Bucket is null");
             return inputEvent;
         }
@@ -39,7 +49,17 @@ public class App implements RequestStreamHandler {
                           objectCreated.getObject()
                                        .getKey()));
 
-        log(String.format("AB03_GLUE_TRANSFORMATION_JOB:%s", System.getenv("AB03_GLUE_TRANSFORMATION_JOB")));
+
+        final String ab03GlueTransformationJobName = System.getenv("AB03_GLUE_TRANSFORMATION_JOB");
+        log(String.format("AB03_GLUE_TRANSFORMATION_JOB:%s",
+                          ab03GlueTransformationJobName));
+
+        glueClient.startJobRun(StartJobRunRequest.builder()
+                                                 .jobName(ab03GlueTransformationJobName)
+                                                 .build());
+
+        log(String.format("Started glue job:%s",
+                          ab03GlueTransformationJobName));
 
         return inputEvent;
     }
@@ -55,12 +75,15 @@ public class App implements RequestStreamHandler {
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
         input.transferTo(baos);
-        log(String.format("input: %s", baos.toString(UTF_8)));
+        log(String.format("input: %s",
+                          baos.toString(UTF_8)));
 
         AWSEvent<ObjectCreated> event = Marshaller.unmarshalEvent(new ByteArrayInputStream(baos.toByteArray()),
                                                                   ObjectCreated.class);
-        Object response = handleEvent(event, context);
-        Marshaller.marshal(output, response);
+        Object response = handleEvent(event,
+                                      context);
+        Marshaller.marshal(output,
+                           response);
     }
 
     public void log(String s) {
